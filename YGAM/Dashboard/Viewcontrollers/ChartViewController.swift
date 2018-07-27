e27d333d53a4b5addc31a8441fc7c835ac333383
Andrew Donnelly <andrew.donnelly@skybettingandgaming.com>
2018-07-27 15:16:42 +0100

    Added touch for chart

--- YGAM/Dashboard/Viewcontrollers/ChartViewController.swift
+++ YGAM/Dashboard/Viewcontrollers/ChartViewController.swift
@@ -16,20 +16,18 @@ class ChartViewController: UIViewController, ChartDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let series1 = ChartSeries([0, 6, 2, 8, 4, 7, 3, 10, 8])
-        series1.color = ChartColors.yellowColor()
-        series1.area = true
-
-        let series2 = ChartSeries([1, 0, 0.5, 0.2, 0, 1, 0.8, 0.3, 1])
-        series2.color = ChartColors.redColor()
-        series2.area = true
-
-        // A partially filled series
-        let series3 = ChartSeries([9, 8, 10, 8.5, 9.5, 10])
-        series3.color = ChartColors.purpleColor()
-
-        chart.add([series1, series2, series3])
-        chart.lineWidth = 4
+        let data = [
+            (x: 0, y: 0),
+            (x: 1, y: 3.1),
+            (x: 4, y: 2),
+            (x: 5, y: 4.2),
+            (x: 7, y: 5),
+            (x: 9, y: 9),
+            (x: 10, y: 8)
+        ]
+        let series = ChartSeries(data: data)
+        chart.add(series)
+        chart.delegate = self
         self.view.addSubview(chart)
     }
 
@@ -43,7 +41,13 @@ class ChartViewController: UIViewController, ChartDelegate {
     }
     
     func didTouchChart(_ chart: Chart, indexes: [Int?], x: Double, left: CGFloat) {
-
+        for (seriesIndex, dataIndex) in indexes.enumerated() {
+            if dataIndex != nil {
+                // The series at `seriesIndex` is that which has been touched
+                let value = chart.valueForSeries(seriesIndex, atIndex: dataIndex)
+                print(value!)
+            }
+        }
     }
 
     func didFinishTouchingChart(_ chart: Chart) {
